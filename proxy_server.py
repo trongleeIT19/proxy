5b8dcfe89392779291e167b7e72976a90529c62f
trongleeIT19 <tronglee.hcmus@gmail.com>
2021-01-01 21:17:02 +0700

    update code

--- proxy_server.py
+++ proxy_server.py
@@ -3,72 +3,55 @@ import threading
 import tkinter as tk
 from tkinter import messagebox
 import os
-import tqdm
 
 BUFFER_SIZE = 4096
 file_index="index.html"
-file_css="style.css"
 index_size=os.path.getsize(file_index)
-css_size=os.path.getsize(file_css)
 
 
-def getFile(a):  
-    #Lay file blacklist.conf
+def getBlacklist(blacklist):  
+#get file blacklist.conf
     f=open("blacklist.conf","rt")
     for x in f:
         a.append(x)
 
 def sendFile(file_name,file_size):
-    
-    progress = tqdm.tqdm(range(file_size), f"Sending {file_name}", unit="B", unit_scale=True, unit_divisor=1024)
     with open(file_name, "rb") as f:
-        for _ in progress:
+        for _ in range(file_size):
             # read the bytes from the file
             bytes_read = f.read(BUFFER_SIZE)
             if not bytes_read:
                 # file transmitting is done
                 break
-            # we use sendall to assure transimission in 
-            # busy networks
-            conn.sendall(b'''HTTP/1.1 \n\n'''+bytes_read)   
-            # update the progress bar
-            progress.update(len(bytes_read))
-
+            conn.sendall(b'''HTTP/1.1 403 Forbidden\n\n'''+bytes_read)   
 
         
-
 def handle_client(conn,a):
-
-    #Lay request la 1 thong diep HTTP
-    
+    #get request as a HTTP message
     request2=conn.recv(10000)
     request=request2.decode('latin-1')
-    print('request:')
+    print(" \n========== Request:==========")
     print(request)
+    
 
-
-    #Lay dong dau thong diep HTTP
+    #Get the first line of HTTP message
     first_line=request.split('\n')[0]
-    print('first_line:')
+    print("========== First_line: ==========")
     print(first_line)
 
 
-    #Lay URL
+    #Get URL
     url=first_line.split(' ')[1]
-    print('url:')
-    print(url)
 
-
-    #find pos of "://"
+    #find position of "://"
     http_pos=url.find("://")
     if http_pos==-1:
         temp=url
     else:
         temp=url[(http_pos+3):]
     
-    print('temp:')
-    print(temp)
-    #find end of web sever
+
+    #find the end of web sever
     websever_pos=temp.find("/")
     if websever_pos==-1:
         websever_pos=len(temp)
@@ -76,7 +59,8 @@ def handle_client(conn,a):
 
     port=80
 
-    #print(url)
+    print("\n\n========== URL:  ==========")
+    print(url)
     websever=temp[:websever_pos]
     port_pos=websever.find(":")
     if port_pos==-1:
@@ -85,15 +69,18 @@ def handle_client(conn,a):
         port=int(websever[port_pos+1:])
         print(port)
         websever=websever[:port_pos]
-    print('websever:')
-    print(websever)
+    
     blocked=False
+    status="Unblocked!"
     for str in a:
         if str[:len(str)-1] == websever:
             blocked=True
+            status="Blocked!"
+    
+    print("\n\n========== Status: ==========")
 
     if blocked==False:
-        print('Unblocked')
+        print(status)
         so=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         so.settimeout(300)
         so.connect((websever,port))
@@ -103,25 +90,26 @@ def handle_client(conn,a):
             if(len(data)>0):
                 conn.sendall(data)
             else:
-                print('out!')
+                #print('out!')
                 break
         so.close()
     else:
-        print('Blocked')
-        sendFile(file_index,index_size)
-        sendFile(file_css,css_size)                                              
-        
+        print(status)
+        sendFile(file_index,index_size)                                              
+    print("\n\n\n")
     conn.close()
 
 
-HOST="192.168.30.174"
+HOST="127.0.0.1"
 PORT=8888
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST,PORT))
 s.listen(1000)
-
+#change working directory
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 a=[]
-getFile(a)  
+getBlacklist(a)  
+print("Waiting for server...")
 
 while True:
     conn,addr=s.accept()
